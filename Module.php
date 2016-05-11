135345b13537dd122808f8ae11deb227f2828524
Tobias Munk <schmunk@usrbin.de>
2016-05-11 15:27:06 +0200

    added module and API controller draft

---
+++ Module.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace hrzg\filefly;
+
+use dmstr\web\traits\AccessBehaviorTrait;
+
+class Module extends \yii\base\Module
+{
+    #use AccessBehaviorTrait;
+
+    public $controllerNamespace = 'hrzg\filefly\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ controllers/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace hrzg\filefly\controllers;
+
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
