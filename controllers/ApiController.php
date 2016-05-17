24b5a6faba7237971c8b1315a289fd5064fc4b24
Chris Stebe <c.stebe@herzogkommunikation.de>
2016-05-17 10:23:12 +0200

    updated API list action, fixed date and add size params

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -6,16 +6,17 @@ use yii\web\HttpException;
 
 class ApiController extends \yii\rest\Controller
 {
-    public function behaviors() {
-        $behaviors = parent::behaviors();
+    public function behaviors()
+    {
+        $behaviors               = parent::behaviors();
         $behaviors['corsFilter'] = [
             'class' => \yii\filters\Cors::className(),
-            'cors' => [
-                'Origin' => ['*'],
-                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                'Access-Control-Request-Headers' => ['*'],
+            'cors'  => [
+                'Origin'                           => ['*'],
+                'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers'   => ['*'],
                 'Access-Control-Allow-Credentials' => true,
-                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Max-Age'           => 86400,
             ],
         ];
         return $behaviors;
@@ -26,15 +27,16 @@ class ApiController extends \yii\rest\Controller
         switch (true) {
             case 'list':
                 $contents = \Yii::$app->fs->listContents();
-                $result = [];
+                $result   = [];
                 foreach ($contents AS $file) {
                     $result[] = [
-                        'name'=>$file['filename'],
-                        'date'=>$file['timestamp'],
-                        'type'=>$file['type'],
+                        'name' => $file['filename'],
+                        'date' => date('Y-m-d H:m:i', $file['timestamp']),
+                        'type' => $file['type'],
+                        'size' => (array_key_exists('size', $file)) ? $file['size'] : null,
                     ];
                 }
-                return ['result'=>$result];
+                return ['result' => $result];
                 break;
             default:
                 throw new HttpException('Not implemented.');
