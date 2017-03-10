dbf461f21acf3118749e290cc8138ffbd6e87e76
Christopher Stebe <c.stebe@herzogkommunikation.de>
2017-03-10 19:16:03 +0100

    updated filefly dashboard

--- views/default/index.php
+++ views/default/index.php
@@ -4,46 +4,44 @@ use yii\helpers\Inflector;
 /*
  * @var yii\web\View $this
  */
+$this->title = 'Filefly';
 ?>
 <h1><?= Inflector::titleize($this->context->module->id) ?></h1>
 <hr>
-<div class="row">
-    <div class="col-xs-12 col-md-4">
-        <h4>
-            <?= \Yii::t('filefly', 'Current Filesystem') ?>
-        </h4>
-        <ul>
-            <li><?= Inflector::titleize($this->context->module->filesystem) ?></li>
-        </ul>
-    </div>
-    <div class="col-xs-12 col-md-4">
-        <h4>RBAC Roles / Permissions</h4>
-        <?php foreach ($this->context->module->accessRoles as $accessRole => $accessPermissions) : ?>
-            <ul>
-                <li>
-                    <?= $accessRole ?>
+<div class="box box-default">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-4">
+                <h4>
+                    <?= \Yii::t('filefly', 'Current Filesystem') ?>
+                </h4>
+                <ul>
+                    <li><?= Inflector::titleize($this->context->module->filesystem) ?></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <h4>RBAC Roles / Permissions</h4>
+                <?php foreach ($this->context->module->accessRoles as $accessRole => $accessPermissions) : ?>
                     <ul>
-                        <?php foreach ($accessPermissions as $accessPermission) : ?>
-                            <li><?= $accessPermission ?></li>
-                        <?php endforeach; ?>
+                        <li>
+                            <?= $accessRole ?>
+                            <ul>
+                                <?php foreach ($accessPermissions as $accessPermission) : ?>
+                                    <li><?= $accessPermission ?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </li>
                     </ul>
-                </li>
-            </ul>
-        <?php endforeach; ?>
-    </div>
-    <div class="col-xs-12 col-md-4">
-        <h4>Access Fields</h4>
-        <?php foreach ($this->context->module->accessFields as $accessField) : ?>
-            <ul>
-                <li><?= $accessField ?>
-                </li>
-            </ul>
-        <?php endforeach; ?>
-    </div>
-</div>
-<div class="row">
-    <div class="col-xs-12">
-        <h4>Outputwidget</h4>
-        <code>echo \hrzg\filemanager\widgets\FileManagerWidget::widget();</code>
+                <?php endforeach; ?>
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <h4>Access Fields</h4>
+                <?php foreach ($this->context->module->accessFields as $accessField) : ?>
+                    <ul>
+                        <li><?= $accessField ?></li>
+                    </ul>
+                <?php endforeach; ?>
+            </div>
+        </div>
     </div>
 </div>
