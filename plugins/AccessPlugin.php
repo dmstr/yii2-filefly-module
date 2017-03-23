35a58f1fc34acbc934ef2b359fc9319c09bfd56f
Christopher Stebe <c.stebe@herzogkommunikation.de>
2017-03-23 12:14:44 +0100

    codedocs, cleanups

--- plugins/AccessPlugin.php
+++ plugins/AccessPlugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link http://www.diemeisterei.de/
- * @copyright Copyright (c) 2016 diemeisterei GmbH, Stuttgart
+ * @copyright Copyright (c) 2017 diemeisterei GmbH, Stuttgart
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,7 +12,6 @@ namespace hrzg\filefly\plugins;
 use League\Flysystem\FilesystemInterface;
 use League\Flysystem\PluginInterface;
 use yii\base\Component;
-
 
 /**
  * Class AccessPlugin
