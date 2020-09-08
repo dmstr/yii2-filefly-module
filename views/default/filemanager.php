f646dc8aae3555faee6d1a1fa689b2bc38c3271f
Elias Luhr <e.luhr@herzogkommunikation.de>
2020-09-08 15:04:27 +0200

    optional thumbnail from api

--- views/default/filemanager.php
+++ views/default/filemanager.php
@@ -9,9 +9,11 @@ if (class_exists(\hrzg\filemanager\widgets\FileManagerWidget::class)) {
     if (isset(Yii::$app->components['settings'])) {
         $thumbnailUrlPrefix = Yii::$app->settings->get('thumbnailUrlPrefix', 'filefly','');
         $thumbnailUrlSuffix = Yii::$app->settings->get('thumbnailUrlSuffix', 'filefly','');
+        $enableThumbnails = Yii::$app->settings->get('enableThumbnails', 'filefly',false);
     } else {
         $thumbnailUrlPrefix = '';
         $thumbnailUrlSuffix = '';
+        $enableThumbnails = false;
     }
 
     echo \hrzg\filemanager\widgets\FileManagerWidget::widget(
@@ -19,6 +21,7 @@ if (class_exists(\hrzg\filemanager\widgets\FileManagerWidget::class)) {
             'handlerUrl' => Url::to('/' . $this->context->module->id . '/api'),
             'thumbnailUrlPrefix' => $thumbnailUrlPrefix,
             'thumbnailUrlSuffix' => $thumbnailUrlSuffix,
+            'enableThumbnails' => $enableThumbnails
         ]
     );
 } else {
