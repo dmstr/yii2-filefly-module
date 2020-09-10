82690e2475b71d0ecdde8a122d63006502570e4c
amk <a.klapper@herzogkommunikation.de>
2020-09-10 09:50:12 +0200

    removed unused thumbnail prefixes and suffixes

--- views/default/filemanager.php
+++ views/default/filemanager.php
@@ -7,13 +7,9 @@ use yii\helpers\Url;
 if (class_exists(\hrzg\filemanager\widgets\FileManagerWidget::class)) {
 
     if (isset(Yii::$app->components['settings'])) {
-        $thumbnailUrlPrefix = Yii::$app->settings->get('thumbnailUrlPrefix', 'filefly','');
-        $thumbnailUrlSuffix = Yii::$app->settings->get('thumbnailUrlSuffix', 'filefly','');
         $enableThumbnails = Yii::$app->settings->get('enableThumbnails', 'filefly',0);
         $enableIconPreviewView = Yii::$app->settings->get('enableIconPreviewView', 'filefly',0);
     } else {
-        $thumbnailUrlPrefix = '';
-        $thumbnailUrlSuffix = '';
         $enableThumbnails = 0;
         $enableIconPreviewView = 0;
     }
@@ -21,8 +17,6 @@ if (class_exists(\hrzg\filemanager\widgets\FileManagerWidget::class)) {
     echo \hrzg\filemanager\widgets\FileManagerWidget::widget(
         [
             'handlerUrl' => Url::to('/' . $this->context->module->id . '/api'),
-            'thumbnailUrlPrefix' => $thumbnailUrlPrefix,
-            'thumbnailUrlSuffix' => $thumbnailUrlSuffix,
             'enableThumbnails' =>  $enableThumbnails,
             'enableIconPreviewView' => $enableIconPreviewView,
         ]
