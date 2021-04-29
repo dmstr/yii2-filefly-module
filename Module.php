bebe26b90f47608c4dbdf4f7171c593b5bfd2e2f
Tobias Munk <schmunk@usrbin.de>
2021-04-29 16:49:17 +0200

    fixed settings type

--- Module.php
+++ Module.php
@@ -174,7 +174,7 @@ class Module extends \yii\base\Module
         \Yii::$app->controller->view->params['breadcrumbs'][] = ['label' => 'Filefly module', 'url' => ['/'.$this->id]];
 
         // Set the allowed MIME types to all files as default
-        \Yii::$app->settings->getOrSet('mime-whitelist', '', 'filefly');
+        \Yii::$app->settings->getOrSet('mime-whitelist', '', 'filefly', 'string');
 
         return true;
     }
