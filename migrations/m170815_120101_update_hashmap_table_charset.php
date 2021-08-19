942c44eb3958cbe52fcc0171616cd42876939836
Elias Luhr <e.luhr@herzogkommunikation.de>
2021-08-19 15:36:10 +0200

    Added support for postgres db

--- migrations/m170815_120101_update_hashmap_table_charset.php
+++ migrations/m170815_120101_update_hashmap_table_charset.php
@@ -8,8 +8,9 @@ class m170815_120101_update_hashmap_table_charset extends Migration
     {
         // to prevent syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
         $this->alterColumn("filefly_hashmap","path","VARCHAR(255) NOT NULL");
-        $this->execute("ALTER TABLE `filefly_hashmap` CONVERT TO CHARACTER SET utf8;");
-    }
+        if ($this->db->getDriverName() === 'mysql') {
+            $this->execute("ALTER TABLE filefly_hashmap CONVERT TO CHARACTER SET utf8;");
+        }    }
 
     public function down()
     {
